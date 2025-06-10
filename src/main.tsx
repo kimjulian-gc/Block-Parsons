e66d8b416de77952b3b0c6d0286da5b3144f4ff6
Julian Kim <kimjulian@grinnell.edu>
2025-06-10 09:37:33 -0500

    Initialize main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import {createRoot} from "react-dom/client";
+
+function throwNull(message: string): never {
+    throw new Error(message);
+}
+
+const root = document.getElementById("root") ?? throwNull("React root cannot be null!");
+
+createRoot(root).render(
+    <>Test</>
+);

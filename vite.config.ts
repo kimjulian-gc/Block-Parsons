eda6fd34af4eeeb326af708556fe1f8590943694
Julian Kim <kimjulian@grinnell.edu>
2025-06-10 09:34:10 -0500

    Setup Vite library mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import {resolve} from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/main.tsx"),
+      formats: ["es"]
+    },
+    copyPublicDir: false
+  },
+  // below is necessary to avoid React using process,
+  // see the Environment Variables box under https://vite.dev/guide/build#css-support
+  define: { "process.env.NODE_ENV": "production" },
 })

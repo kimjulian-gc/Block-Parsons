f7c1b0d30782cf39d0ed7983866e78621e15b67b
Julian Kim <kimjulian@grinnell.edu>
2025-06-12 15:47:12 -0500

    Start work on UI layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { Block } from "./block/Block.tsx";
-import { DndContext, pointerWithin, rectIntersection } from "@dnd-kit/core";
+import { App } from "./app/App.tsx";
 
 function throwNull(message: string): never {
   throw new Error(message);
@@ -9,30 +8,4 @@ function throwNull(message: string): never {
 const root =
   document.getElementById("root") ?? throwNull("React root cannot be null!");
 
-const blocks = [
-  { name: "small-grey" },
-  {
-    name: "solid-circle",
-    argumentOptions: { minAmount: 2 },
-    childBlocks: [{ name: "20" }, { name: '"red"' }],
-  },
-];
-
-createRoot(root).render(
-  <DndContext
-    collisionDetection={(args) => {
-      const pointerCollisions = pointerWithin(args);
-
-      if (pointerCollisions.length > 0) {
-        return pointerCollisions;
-      }
-      return rectIntersection(args);
-    }}
-  >
-    <Block
-      name={"define"}
-      argumentOptions={{ minAmount: 2 }}
-      childBlocks={blocks}
-    />
-  </DndContext>,
-);
+createRoot(root).render(<App />);

cd93a9f04dfb2bbbd969425234113536b4bf6709
Julian Kim <kimjulian@grinnell.edu>
2025-06-12 16:20:04 -0500

    Replace Node Crypto UUID with uuid
    Fixes https://vite.dev/guide/troubleshooting.html#module-externalized-for-browser-compatibility

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,22 +6,22 @@ import {
 } from "@dnd-kit/core";
 import { Block, type BlockProps } from "../block/Block.tsx";
 import { useState } from "react";
-import { randomUUID } from "node:crypto";
+import { v4 as uuidv4 } from "uuid";
 
 const startingBlocks: BlockProps[] = [
   {
-    id: randomUUID(),
+    id: uuidv4(),
     name: "define",
     argumentOptions: { minAmount: 2 },
     childBlocks: [
-      { id: randomUUID(), name: "small-grey" },
+      { id: uuidv4(), name: "small-grey" },
       {
-        id: randomUUID(),
+        id: uuidv4(),
         name: "solid-circle",
         argumentOptions: { minAmount: 2 },
         childBlocks: [
-          { id: randomUUID(), name: "20" },
-          { id: randomUUID(), name: '"red"' },
+          { id: uuidv4(), name: "20" },
+          { id: uuidv4(), name: '"red"' },
         ],
       },
     ],

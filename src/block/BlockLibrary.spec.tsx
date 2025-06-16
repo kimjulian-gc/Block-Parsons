43ec70aa771c86b9473a084c16ddb007cd13f16c
Elene Sturua <sturuael@grinnell.edu>
2025-06-16 12:21:31 -0500

    aria-label, spacing between blocks, allow arbitrary amount of blocks added

---
+++ src/block/BlockLibrary.spec.tsx
@@ -0,0 +1,32 @@
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+//import { Component } from "react";
+import { BlockLibrary } from "./BlockLibrary.tsx";
+//import { BlockLabels } from "./aria-labels.ts";
+
+
+
+describe("Block component", () => {
+  it("renders block library", () => {
+    render(<BlockLibrary />);
+    expect(screen.getByText(/Library Box/i)).toBeInTheDocument();
+  });
+
+  it(" renders solutionBox", () => {
+    render(<BlockLibrary />);
+    expect(screen.getByText(/Solution Box/i)).toBeInTheDocument();
+
+  });
+  it("renders example blocks inside the block library", () =>{
+    render(<BlockLibrary />);
+    expect(screen.getByText(/small-circle/i)).toBeInTheDocument();
+    expect(screen.getByText(/medium-rectangle/i)).toBeInTheDocument();
+
+  });
+  it("renders correct number of example blocks inside the block library", () =>{
+    render(<BlockLibrary />);
+    const blocks = screen.getAllByTestId("Block");
+    expect(blocks.length).toBe(5);
+
+  });
+});
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
-import { Block } from "./block/Block.tsx";
+// import { Block } from "./block/Block.tsx";
+import { BlockLibrary } from "./block/BlockLibrary.tsx";
 
 function throwNull(message: string): never {
   throw new Error(message);
@@ -8,19 +9,20 @@ function throwNull(message: string): never {
 const root =
   document.getElementById("root") ?? throwNull("React root cannot be null!");
 
-const blocks = [
-  { name: "small-grey" },
-  {
-    name: "solid-circle",
-    argumentOptions: { minAmount: 3, expandable: true },
-    childBlocks: [{ name: "20" }, { name: "red" }, { name: "blue" }],
-  },
-];
+// const blocks = [
+//   { name: "small-grey" },
+//   {
+//     name: "solid-circle",
+//     argumentOptions: { minAmount: 3, expandable: true },
+//     childBlocks: [{ name: "20" }, { name: "red" }, { name: "blue" }],
+//   },
+// ];
 
-createRoot(root).render(
-  <Block
-    name={"define"}
-    argumentOptions={{ minAmount: 2 }}
-    childBlocks={blocks}
-  />,
-);
+createRoot(root).render(<BlockLibrary />);
+// createRoot(root).render(
+//   <Block
+//     name={"define"}
+//     argumentOptions={{ minAmount: 2 }}
+//     childBlocks={blocks}
+//   />,
+// );

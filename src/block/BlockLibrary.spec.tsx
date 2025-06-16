f3c74d8dc7fc2b4071d139d2f8ae2b2fb689645d
Elene Sturua <sturuael@grinnell.edu>
2025-06-16 13:53:50 -0500

    data-test-id removed, prettier run

--- src/block/BlockLibrary.spec.tsx
+++ src/block/BlockLibrary.spec.tsx
@@ -4,8 +4,6 @@ import { render, screen } from "@testing-library/react";
 import { BlockLibrary } from "./BlockLibrary.tsx";
 //import { BlockLabels } from "./aria-labels.ts";
 
-
-
 describe("Block component", () => {
   it("renders block library", () => {
     render(<BlockLibrary />);
@@ -15,18 +13,15 @@ describe("Block component", () => {
   it(" renders solutionBox", () => {
     render(<BlockLibrary />);
     expect(screen.getByText(/Solution Box/i)).toBeInTheDocument();
-
   });
-  it("renders example blocks inside the block library", () =>{
+  it("renders example blocks inside the block library", () => {
     render(<BlockLibrary />);
     expect(screen.getByText(/small-circle/i)).toBeInTheDocument();
     expect(screen.getByText(/medium-rectangle/i)).toBeInTheDocument();
-
   });
-  it("renders correct number of example blocks inside the block library", () =>{
+  it("renders correct number of example blocks inside the block library", () => {
     render(<BlockLibrary />);
     const blocks = screen.getAllByTestId("Block");
     expect(blocks.length).toBe(5);
-
   });
 });

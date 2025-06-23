53ce9698ed2356b12b78784b6689bde5b6d9a6a0
Julian Kim <kimjulian@grinnell.edu>
2025-06-23 09:50:16 -0500

    Streamline UI structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom/client";
-import { SolutionBox } from "./solution-box/SolutionBox.tsx";
 import { throwNull } from "./common/utils.ts";
+import { App } from "./app/App.tsx";
 
 const root =
   document.getElementById("root") ?? throwNull("React root cannot be null!");
 
-createRoot(root).render(<SolutionBox />);
+createRoot(root).render(<App />);

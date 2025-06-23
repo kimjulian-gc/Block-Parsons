d54f330fd37df7fe55889f4d7cd1b2be51155406
Julian Kim <kimjulian@grinnell.edu>
2025-06-23 15:03:30 -0500

    Refactor BlockContext

--- src/app/BlockContext.tsx
+++ src/app/BlockContext.tsx
@@ -1,19 +1,20 @@
 import { createContext, useContext } from "react";
-import type { Block } from "../block/Block.tsx";
+import { newUUID } from "../common/utils.ts";
 
 interface ArgumentOptions {
   minAmount: number;
   expandable?: boolean;
 }
 
-export interface Block {
+export interface BlockData {
   name: string;
   argumentOptions?: ArgumentOptions;
   childBlocks?: string[];
+  parentId: string | null;
 }
 
 interface BlockContextType {
-  blocks: Map<string, Block>;
+  blocks: Map<string, BlockData>;
 }
 
 export const BlockContext = createContext<BlockContextType | null>(null);
@@ -25,3 +26,32 @@ export function useBlockContext() {
   }
   return context;
 }
+
+const defineId = newUUID();
+const smallGreyId = newUUID();
+const solidCircleId = newUUID();
+const twentyId = newUUID();
+const redId = newUUID();
+export const startingBlockMap = new Map<string, BlockData>([
+  [
+    defineId,
+    {
+      name: "define",
+      argumentOptions: { minAmount: 2 },
+      childBlocks: [smallGreyId, solidCircleId],
+      parentId: null,
+    },
+  ],
+  [smallGreyId, { name: "small-grey", parentId: defineId }],
+  [
+    solidCircleId,
+    {
+      name: "solid-circle",
+      argumentOptions: { minAmount: 2 },
+      childBlocks: [twentyId, redId],
+      parentId: defineId,
+    },
+  ],
+  [twentyId, { name: "20", parentId: solidCircleId }],
+  [redId, { name: '"red"', parentId: solidCircleId }],
+]);

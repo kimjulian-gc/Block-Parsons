f6822e2de3eb77cd70be2f5544ac3dd81972708e
Julian Kim <kimjulian@grinnell.edu>
2025-06-24 14:31:15 -0500

    Replace DFS drag-and-drop update logic with reducer-based approach

--- src/app/providers/block/block-reducer.ts
+++ src/app/providers/block/block-reducer.ts
@@ -1,5 +1,6 @@
 import { type BlockData, RootParents } from "./BlockContext.ts";
 import { Map } from "immutable";
+import { ArgumentSlotPrefix } from "../../../block/ArgumentSlot.tsx";
 
 const defineId = "define-id";
 const smallGreyId = "smallgrey-id";
@@ -40,49 +41,77 @@ export const startingBlockMap = Map<string, BlockData>([
   [fiveId, { name: "5", parentId: RootParents.BlockLibrary }],
 ]);
 
-export type BlockDispatchType =
-  | { type: "libraryRoot"; payload: string }
-  | { type: "solutionRoot"; payload: string }
-  | { type: "parent"; payload: { id: string; parentId: string } };
+export type BlockDispatchType = {
+  type: "SET_PARENT";
+  payload: { id: string; parentId: string };
+};
 
 export function blockReducer(
   state: typeof startingBlockMap,
   action: BlockDispatchType,
 ) {
   switch (action.type) {
-    case "libraryRoot": {
-      const block = state.get(action.payload);
-      if (!block) {
-        return state;
-      }
-      block.parentId = RootParents.BlockLibrary;
-      return state.set(action.payload, block);
-    }
-    case "solutionRoot": {
-      const block = state.get(action.payload);
-      if (!block) {
-        return state;
-      }
-      block.parentId = RootParents.SolutionBox;
-      return state.set(action.payload, block);
-    }
-    case "parent": {
+    // TODO: add more cases, for now ignore eslint
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    case "SET_PARENT": {
       const { id, parentId } = action.payload;
+      console.log(id, parentId);
       const child = state.get(id);
-      const parent = state.get(parentId);
       if (!child) {
-        return state;
+        throw new Error(`attempted to set parent of unknown block ${id}`);
       }
-      if (!parent) {
-        return state;
+      // remove child from original parent's child blocks
+      const ogParentId = child.parentId;
+      const ogParent = state.get(ogParentId);
+      const ogChildBlocks = ogParent?.childBlocks;
+      let ogIndex: number | null = null;
+      if (ogChildBlocks && (ogIndex = ogChildBlocks.indexOf(id)) > -1) {
+        console.log("removing from ogChildBlocks");
+        // if (ogIndex === ogChildBlocks.length - 1) {
+        //   ogChildBlocks.pop();
+        // } else {
+        ogChildBlocks[ogIndex] = null;
+        // }
+        ogParent.childBlocks = ogChildBlocks;
       }
-      child.parentId = parentId;
-      // update parent's child blocks
-      const parentChildBlocks = parent.childBlocks ?? [];
-      parentChildBlocks.push(id);
-      parent.childBlocks = parentChildBlocks;
 
-      return state.set(id, child).set(parentId, parent);
+      // set new parent
+      const [prefix, newParentId, slotIndex] = parentId.split(":");
+      if (!prefix.startsWith(ArgumentSlotPrefix)) {
+        // top-level block
+        console.warn("new parent top level", prefix);
+        child.parentId = RootParents.SolutionBox;
+        return state.set(id, child);
+      }
+      // set new parent
+      child.parentId = newParentId;
+      const updatedChildState = state.set(id, child);
+      const newParent = state.get(newParentId);
+      if (!newParent || !slotIndex) {
+        throw new Error("new parent not found?");
+      }
+      const parentChildBlocks = newParent.childBlocks ?? [];
+      // potential swap
+      const tempId = parentChildBlocks[Number(slotIndex)];
+      parentChildBlocks[Number(slotIndex)] = id;
+      let temp: BlockData | undefined;
+      let afterTempState = updatedChildState;
+      if (tempId && (temp = updatedChildState.get(tempId))) {
+        console.log("swapping", ogIndex);
+        if (ogChildBlocks && ogIndex !== null && ogIndex > -1) {
+          // update ogChildBlocks
+          console.warn("updating ogChildBlocks");
+          ogChildBlocks[ogIndex] = tempId;
+          ogParent.childBlocks = ogChildBlocks;
+        }
+        temp.parentId = ogParentId;
+        afterTempState = afterTempState.set(tempId, temp);
+      }
+      newParent.childBlocks = parentChildBlocks;
+
+      console.log(afterTempState);
+
+      return afterTempState.set(newParentId, newParent);
     }
   }
 }
--- src/app/providers/drag-and-drop/DndProvider.tsx
+++ src/app/providers/drag-and-drop/DndProvider.tsx
@@ -3,6 +3,7 @@ import { type PropsWithChildren, useCallback, useState } from "react";
 import {
   defaultKeyboardCoordinateGetter,
   DndContext,
+  type DragEndEvent,
   DragOverlay,
   type DragStartEvent,
   KeyboardSensor,
@@ -11,6 +12,7 @@ import {
   useSensors,
 } from "@dnd-kit/core";
 import { collisionDetection } from "../../../solution-box/solution-box-utils.ts";
+import { useBlockDispatchContext } from "../block/BlockDispatchContext.ts";
 
 export function DndProvider({ children }: PropsWithChildren) {
   const [activeId, setActiveId] = useState<string | null>(null);
@@ -22,24 +24,26 @@ export function DndProvider({ children }: PropsWithChildren) {
     }),
   );
 
-  const handleDragEnd = useCallback(() => {
-    // 1. find the original parent block,
-    // 2. remove the active block from the original parent, and
-    // 3. push to its new parent's child blocks
-    // const child =
-    //   blocks.get(active.id.toString()) ??
-    //   throwNull("child was somehow not found?");
-    // const parentSlotDetails = over?.id.toString().split(":") ?? null;
-    // const newParent = parentSlotDetails?.[1] ?? over?.id.toString() ?? null;
-    // const argumentSlot = parentSlotDetails?.[2] ?? null;
-    // const newTopLevel = removedAndPushed(
-    //   topLevelBlocks,
-    //   child,
-    //   newParent,
-    //   argumentSlot ? Number(argumentSlot) : null,
-    // );
-    setActiveId(null);
-  }, []);
+  const dispatch = useBlockDispatchContext();
+
+  const handleDragEnd = useCallback(
+    ({ active, over }: DragEndEvent) => {
+      // 1. find the original parent block,
+      // 2. remove the active block from the original parent, and
+      // 3. push to its new parent's child blocks
+      const newParentId = over?.id.toString();
+      if (!newParentId) {
+        return;
+      }
+      dispatch({
+        type: "SET_PARENT",
+        payload: { id: active.id.toString(), parentId: newParentId },
+      });
+
+      setActiveId(null);
+    },
+    [dispatch],
+  );
 
   const handleDragStart = useCallback(({ active }: DragStartEvent) => {
     setActiveId(active.id.toString());

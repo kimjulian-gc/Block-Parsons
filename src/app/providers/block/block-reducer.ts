35d4c379b58c6e921b551c3a519ba051c01cd4ef
Julian Kim <kimjulian@grinnell.edu>
2025-06-25 10:15:33 -0500

    Extract startingBlockMap to initial-state module

--- src/app/providers/block/block-reducer.ts
+++ src/app/providers/block/block-reducer.ts
@@ -1,46 +1,6 @@
-import { type BlockData } from "./BlockContext.ts";
-import { Map } from "immutable";
 import { ArgumentSlotPrefix } from "../../../block/ArgumentSlot.tsx";
 import { SectionTitles } from "../../../common/utils.ts";
-
-const defineId = "define-id";
-const smallGreyId = "smallgrey-id";
-const solidCircleId = "solidcircle-id";
-const twentyId = "twenty-id";
-const redId = "red-id";
-const squareId = "square-id";
-const fiveId = "five-id";
-export const startingBlockMap = Map<string, BlockData>([
-  [
-    defineId,
-    {
-      name: "define",
-      argumentOptions: { minAmount: 2 },
-      childBlocks: [smallGreyId, solidCircleId],
-      parentId: SectionTitles.SolutionBox,
-    },
-  ],
-  [
-    smallGreyId,
-    {
-      name: "small-grey",
-      parentId: defineId,
-    },
-  ],
-  [
-    solidCircleId,
-    {
-      name: "solid-circle",
-      argumentOptions: { minAmount: 2 },
-      childBlocks: [twentyId, redId],
-      parentId: defineId,
-    },
-  ],
-  [twentyId, { name: "20", parentId: solidCircleId }],
-  [redId, { name: '"red"', parentId: solidCircleId }],
-  [squareId, { name: "square", parentId: SectionTitles.BlockLibrary }],
-  [fiveId, { name: "5", parentId: SectionTitles.BlockLibrary }],
-]);
+import type { startingBlockMap } from "./initial-state.ts";
 
 export type BlockDispatchType = {
   type: "SET_PARENT";

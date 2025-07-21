9550e5206dafebacb62dca32e09567d98cbe689b
Elene Sturua <sturuael@grinnell.edu>
2025-07-21 14:25:51 -0500

    - merged

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { throwNull } from "./common/utils.ts";
 import { App } from "./app/App.tsx";
 import { enableMapSet } from "immer";
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 enableMapSet();
 
 const root =
